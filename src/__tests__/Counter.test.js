0b813f5da34539d97d408d2a285cf39e1242c47c
Willy Hardian <hardian.will@gmail.com>
2023-07-02 16:07:50 +0700

    add test

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -1,5 +1,14 @@
+import { fireEvent, render, screen } from "@testing-library/react"
 import Counter from "../components/Counter"
+import '@testing-library/jest-dom'
 
 test('Rendering counter correctly', () => {
     render(<Counter />)
+    const title = screen.getByText(/counter/i)
+    expect(title).toBeInTheDocument();
+    const btn = screen.getByRole('button')
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveTextContent(/count is/i)
+    fireEvent.click(btn)
+
 })
