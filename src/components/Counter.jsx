4f9d9c01b8b18e5ba015725176769c7feb9f1cd4
Willy Hardian <hardian.will@gmail.com>
2023-07-02 21:18:07 +0700

    delete image and svg

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,10 @@
 import { useState } from 'react'
 import './Counter.css'
-import reactLogo from '../assets/react.svg'
-// import viteLogo from '/vite.svg'
 
 export default function Counter() {
     const [count, setCount] = useState(0)
     return (
         <>
-            <div>
-                {/* <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a> */}
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
             <h1>Counter</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
